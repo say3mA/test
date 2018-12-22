f8663e181ecd510f61a09def0e37c03f88f84342
MacUser <MacUser@macusercbook138.fios-router.home>
2018-12-22 14:07:37 -0500

    addding stuff

---
+++ dbtest/src/main.java
@@ -0,0 +1,28 @@
+import java.sql.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class main {
+	public static void main(String[] args) throws Exception {
+		/**Class.forName("com.mysql.jdbc.Driver");
+		String url = "jdbc:mysql://localhost:3306/sampledb";
+		String uname = "root";
+		String pass = "";
+		String query = "select name from sampletable where id = 1;";
+		Connection con = DriverManager.getConnection(url, uname, pass);
+		Statement st = con.createStatement();
+		ResultSet rs = st.executeQuery(query);
+		rs.next();
+		String name = rs.getString("name");
+		
+		System.out.println(name);
+		
+		con.close();
+		st.close();
+		
+		
+	}
+}
+**/
+		
